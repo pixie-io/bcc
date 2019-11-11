39fc886e4c91375150bdf4405b5b7c2c33e50b43
Yaxiong Zhao <yzhao@pixielabs.ai>
2019-11-11 11:31:36 -0800

    Shorten a name that is too long for attaching uprobe
    Done by remove certain suffix, and attach a 16 chars random string.

--- src/cc/api/BPF.cc
+++ src/cc/api/BPF.cc
@@ -23,6 +23,7 @@
 #include <fcntl.h>
 #include <iostream>
 #include <memory>
+#include <random>
 #include <sstream>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -853,6 +854,34 @@ bool BPF::add_module(std::string module)
     false : true;
 }
 
+namespace {
+
+std::string random_alnum_string(int len) {
+  static constexpr char kDict[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+  static std::random_device rd;
+  std::uniform_int_distribution<size_t> dist(0, sizeof(kDict)-1);
+  std::string res;
+  res.reserve(len);
+  for (int i = 0; i < len; ++i) {
+    res.push_back(kDict[dist(rd)]);
+  }
+  return res;
+}
+
+constexpr size_t kEventNameSizeLimit = 224;
+
+std::string shorten_event_name(const std::string& name) {
+  constexpr size_t kRandomSuffixLen = 16;
+  std::string res;
+  res.reserve(kEventNameSizeLimit);
+  res.assign(name);
+  res.resize(kEventNameSizeLimit - kRandomSuffixLen);
+  res.append(random_alnum_string(kRandomSuffixLen));
+  return res;
+}
+
+}  // namespace
+
 std::string BPF::get_uprobe_event(const std::string& binary_path,
                                   uint64_t offset, bpf_probe_attach_type type,
                                   pid_t pid) {
@@ -861,6 +890,15 @@ std::string BPF::get_uprobe_event(const std::string& binary_path,
   res += "_0x" + uint_to_hex(offset);
   if (pid != -1)
     res += "_" + std::to_string(pid);
+  if (res.size() > kEventNameSizeLimit) {
+    auto iter = name_map_.find(res);
+    if (iter != name_map_.end()) {
+      return iter->second;
+    }
+    std::string shortend_name = shorten_event_name(res);
+    name_map_[res] = shortend_name;
+    return shortend_name;
+  }
   return res;
 }
 
