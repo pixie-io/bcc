bea0fc8a8dedc072f7a1ce790ad0ab435f5c9c78
Ryan Cheng <rcheng@pixielabs.ai>
2023-01-18 10:41:16 -0800

    Revert "Fix random_alnum_string in BPF.cc."
    This reverts commit 44d4590fba68634f0ea4ce897383abf957b67102.

--- src/cc/api/BPF.cc
+++ src/cc/api/BPF.cc
@@ -856,14 +856,14 @@ bool BPF::add_module(std::string module)
 
 namespace {
 
-std::string random_alnum_string(const int len) {
+std::string random_alnum_string(int len) {
   static constexpr char kDict[] = "0123456789abcdefghijklmnopqrstuvwxyz";
-  static std::mt19937 gen;
-  static std::uniform_int_distribution<size_t> dist(0, sizeof(kDict) - 2);
+  static std::random_device rd;
+  std::uniform_int_distribution<size_t> dist(0, sizeof(kDict)-1);
   std::string res;
-  res.resize(len);
+  res.reserve(len);
   for (int i = 0; i < len; ++i) {
-    res[i] = kDict[dist(gen)];
+    res.push_back(kDict[dist(rd)]);
   }
   return res;
 }
